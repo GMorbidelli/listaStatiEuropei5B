4fbb0e767fc5eb83f22b3f1f67cf2fc0dbbe4a96
GMorbidelli <morbidelligabriele03@gmail.com>
2021-12-03 16:00:59 +0100

    array di stati

--- app/src/main/java/com/example/lisstastatieuropei5b/MainActivity.java
+++ app/src/main/java/com/example/lisstastatieuropei5b/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.lisstastatieuropei5b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lwStati;
+    String listastati[] ={"Italia,", "Francia", "Germania", "Spagan"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lwStati = (ListView) findViewById(R.id.listaStati);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.activity_main, R.id.textView, listastati);
+        lwStati.setAdapter(aa);
+
+
+
     }
 }
